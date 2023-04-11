ad8be68560a65e50ff053dfcab3d2bd3f85efc4b
Eoin Quinn <eoin.quinn55@mail.dcu.ie>
2023-04-11 14:28:28 +0100

    Fixing navigation bar

--- simple_test_root/polls/views.py
+++ simple_test_root/polls/views.py
@@ -13,6 +13,10 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
 
 
 # Create your views here.
